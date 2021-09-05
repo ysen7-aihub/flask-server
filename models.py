93622082efa2074f5efd3812c9a194d8a813d5d8
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-09-05 23:09:28 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy() # app.py에서 연결된 db 객체가 아닐 듯
 
 class Diary(db.Model):
-
     __table_name__ = 'diary'
 
     id = db.Column(db.Integer, primary_key= True, autoincrement=True) # diary 테이블의 pk
@@ -26,3 +25,16 @@ class Music(db.Model):
     def __repr__(self):
         return f"<Diary('{self.id}', '{self.txt}', '{self.img}')>"
 
+# TODO: 예측 결과 테이블 만들기
+class Predict(db.Model):
+    __table_name__ = 'predict'
+
+    id = db.Column(db.Integer, primary_key= True, autoincrement=True)
+    score1 = db.Column(db.Float(30), nullable=False) # nlp 결과
+    score2 = db.Column(db.String(30), nullable=False) # cnn 결과
+    # 사용자로부터 받은 label, 음악추천 모델 재학습 용도, 우선 nlp 감정 6개? 중 하나로 받을 생각이기 때문에 string으로 해놨음
+    label = db.Column(db.String, nullable=True)
+
+    # 사용자가 이해하는 객체의 모습을 표현, 출력 표현 리턴
+    def __repr__(self):
+        return f"<Predict('{self.id}', '{self.score1}', '{self.score2}',  '{self.label}')>"
