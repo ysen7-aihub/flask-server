f6ecf3fbf93da8d44f09196b61420a39718a9c90
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-09-13 22:33:16 +0900

    Update Footer.jsx

--- frontend/moodmaker/src/components/Footer.jsx
+++ frontend/moodmaker/src/components/Footer.jsx
@@ -11,19 +11,7 @@ function Footer() {
           <p>github</p>
         </a>
       </div>
-      <div className="footer">
-        <Link to="/" smooth={true.toString()}>
-          <p>Home</p>
-        </Link>
 
-        <ScrollLink to="services" smooth={true}>
-          <p>Projects</p>
-        </ScrollLink>
-
-        <ScrollLink to="features" smooth={true}>
-          <p>Upload</p>
-        </ScrollLink>
-      </div>
       <p className="footer-p">
         Team 양재역7번출구 | Made using React{" "}
         <span role="img" aria-label="atom">
@@ -31,9 +19,6 @@ function Footer() {
         </span>
       </p>
       <br />
-      <ScrollLink to="topofpage" smooth={true} className="to-top-btn">
-        <p>위로 올라가기</p>
-      </ScrollLink>
     </div>
   );
 }
