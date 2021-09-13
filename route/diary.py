46f860c5c544eec948ba01900fbf7e1dd59a0fa9
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-09-13 23:40:36 +0900

    Update diary.py

--- route/diary.py
+++ route/diary.py
@@ -1,11 +1,9 @@
 from flask_restx import Resource, Api, Namespace, fields
 from flask import Blueprint, request, jsonify, flash, Flask, redirect, url_for
 from werkzeug.utils import secure_filename
-from annoyUses import SpotifyRecommend
 from connect_RDS import get_secret
 import os
 import requests
-import time
 
 conn, cursor = get_secret()
 img = {}
@@ -13,15 +11,18 @@ txt = {}
 count = 1
 
 UPLOAD_FOLDER = "./uploaded_img"
+print('os.getenv:', os.getenv('UPLOAD_FOLDER'))
 
 Diary = Namespace(name='Diary',
                   description="Diary의 이미지와 텍스트 리스트를 작성하기 위해 사용하는 API")
 
 diary_txt = Diary.model('Diary', {
+    # diary list Model 객체 생성
     'txt': fields.String(description='txt', required=True, example="수요일은 특식 데이이다. 학교 급식에서 잔반 없는 날...")
 })
 
 diary_img = Diary.model('Diary', {
+    # diary list Model 객체 생성
     'img': fields.String(description='img', required=True,
                          example="https://image.rocketpunch.com/company/5466/naver_logo.png?s=400x400&t=inside"),
 
@@ -42,7 +43,7 @@ class DiaryPost(Resource):
     @Diary.response(201, 'Success', diary_txt_with_id)
     @Diary.response(400, 'Fail')
     def post(self):
-        """서버에 Diary 텍스트 등록"""
+        """서버에 Diary 리스트를 등록"""
         global txt
 
         txt = request.json.get('txt');
@@ -55,7 +56,7 @@ class DiaryPost(Resource):
     @Diary.response(201, 'Success', diary_img_with_id)
     @Diary.response(400, 'Fail')
     def post(self):
-        """서버에 Diary 이미지 등록"""
+        """서버에 Diary 리스트를 등록"""
         global count
         global img
         global txt
@@ -78,10 +79,11 @@ class DiaryPost(Resource):
 
 
         try:
-            cnn = requests.post('http://13.125.225.154/predict/img', files=img)
+            cnn = requests.post('http://13.125.225.154:5000/predict/img', files=img)
 
-            nlp = requests.post('http://3.34.91.153/predict/txt', txt=txt).json()
+            nlp = requests.post('http://3.34.91.153:5000/predict/txt', txt=txt).json()
 
+            # dB에 저장
             query = """
                     INSERT INTO moodmaker.predict (sen_1, sen_2, sen_3, score_1, score_2, score_3, cnn_score)
                     VALUES ('{0}','{1}','{2}','{3}','{4}','{5}');""".format(nlp.sen_1,
@@ -96,13 +98,13 @@ class DiaryPost(Resource):
             return "fail!"
 
 
-"""
+
 @Diary.route('/<int:diary_id>')
 @Diary.doc(params={'diary_id': 'A Unique ID'}) # 설명 추가
 class DiarySimple(Resource): # 이름 수정
-    @Diary.response(201, 'Success', diary_fields_with_id) # 특정 스키마가 반환된다.
+    @Diary.response(201, 'Success', diary_txt_with_id) # 특정 스키마가 반환된다.
     def get(self, diary_id):
-        Diary 리스트에 id와 일치하는 ID를 가진 값을 반환
+        """Diary 리스트에 id와 일치하는 ID를 가진 값을 반환'"""
         return {
             'diary_id': diary_id,
             'img': img[diary_id],
@@ -111,7 +113,7 @@ class DiarySimple(Resource): # 이름 수정
     @Diary.doc(response={202:'Success'})
     @Diary.doc(response={500:'Failed'})
     def put(self, diary_id):
-        Diary 리스트에 id와 일치하는 ID를 찾아 내용을 수정
+        """Diary 리스트에 id와 일치하는 ID를 찾아 내용을 수정"""
         img[diary_id] = request.json.get('img')
         txt[diary_id] = request.json.get('txt')
         return {
@@ -121,11 +123,10 @@ class DiarySimple(Resource): # 이름 수정
         }, 202
 
     def delete(self, diary_id):
-        Diary 리스트에 id와 일치하는 ID를 가진 내용을 삭제
+        """Diary 리스트에 id와 일치하는 ID를 가진 내용을 삭제"""
         del txt[diary_id]
         del img[diary_id]
         return {
             "delete": "success"
         }, 202
 
-"""
