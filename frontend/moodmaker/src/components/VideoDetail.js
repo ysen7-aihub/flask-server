c730bdc6e526ae4dc982f8c8b74b8af22d7fb743
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-09-13 22:37:50 +0900

    Update VideoDetail.js

--- frontend/moodmaker/src/components/VideoDetail.js
+++ frontend/moodmaker/src/components/VideoDetail.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  /*
   console.log(video);
   if (!video) {
     return <div>Loading...</div>;
   }
-  */
 
   //const videoId = video.id.videoId;
   //const src = `https://www.youtube.com/embed/${videoId}`;
-  const src = "https://www.youtube.com/embed/tgbNymZ7vqY";
+  //const src = "https://www.youtube.com/embed/tgbNymZ7vqY";
   return (
     <div className="video-container">
-      <iframe className="responsive-iframe" src={src}></iframe>
-      <div className="details">{/* <div>{video.snippet.title}</div> */}</div>
+      <iframe className="responsive-iframe" src={video}></iframe>
+      <div className="details">
+        {" "}
+        <div>{/*{video.snippet.title}*/}</div>{" "}
+      </div>
     </div>
   );
 };
